22c984e43b5efc490ba7b07019be10a87a498828
root <rehamhakeem.official@gmail.com>
2024-07-31 11:33:01 -0700

    all tasks

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,32 +1,23 @@
 #!/usr/bin/python3
-"""Defines a class Student."""
+"""Module implementing class with method to serialize itself"""
 
 
 class Student:
-    """Represent a student."""
-
+    """Class to represent student"""
     def __init__(self, first_name, last_name, age):
-        """Initialize a new Student.
-
-        Args:
-            first_name (str): The first name of the student.
-            last_name (str): The last name of the student.
-            age (int): The age of the student.
-        """
+        """Initialize new student instance with name and age"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """Get a dictionary representation of the Student.
+        """Create copy of attributes for use in json string
 
-        If attrs is a list of strings, represents only those attributes
-        included in the list.
-
-        Args:
-            attrs (list): (Optional) The attributes to represent.
+        If `attrs` is not None, use `attrs` list to select desired attributes.
         """
-        if (type(attrs) == list and
-                all(type(ele) == str for ele in attrs)):
-            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
-        return self.__dict__
+        if attrs is None or not isinstance(attrs, (list, tuple)):
+            return self.__dict__.copy()
+        else:
+            ret = {k: v for k, v in filter(lambda x: x[0] in attrs,
+                                           self.__dict__.items())}
+            return ret
