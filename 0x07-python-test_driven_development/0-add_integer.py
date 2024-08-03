7bf772d8199e939fa95074853772604da240c1e7
root <rehamhakeem.official@gmail.com>
2024-08-03 09:15:50 -0700

    task 0 edited

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,92 +1,18 @@
-#dd_integer` Module
-==========================
-
-This module provides a function `add_integer` that adds two numbers.
-
-Using `add_integer`
--------------------
-
-Importing the function from the module:
->>> add_integer = __import__('0-add_integer').add_integer
-
-Addition Examples:
->>> add_integer(4, 4)
-8
-
->>> add_integer(-40, -60)
--100
-
->>> add_integer(-10, 5)
--5
-
->>> add_integer(4.0, 4.0)
-8
-
->>> add_integer(2)
-100
-
-Handling Invalid Inputs:
->>> add_integer(10, "hello")
-Traceback (most recent call last):
-    ...
-TypeError: b must be an integer
-
->>> add_integer(None)
-Traceback (most recent call last):
-    ...
-TypeError: a must be an integer
-
->>> add_integer('1', 5)
-Traceback (most recent call last):
-    ...
-TypeError: a must be an integer
-
->>> add_integer('abc', 'def')
-Traceback (most recent call last):
-    ...
-TypeError: a must be an integer
-
->>> add_integer((1, 2, 3))
-Traceback (most recent call last):
-    ...
-TypeError: a must be an integer
-
->>> add_integer(10, [0])
-Traceback (most recent call last):
-    ...
-TypeError: b must be an integer
-
->>> add_integer("Add")
-Traceback (most recent call last):
-    ...
-TypeError: a must be an integer
-
-Special Cases:
->>> add_integer(float('inf'), 0)
-Traceback (most recent call last):
-    ...
-OverflowError: cannot convert float infinity to integer
-
->>> add_integer(float('inf'), float('-inf'))
-Traceback (most recent call last):
-    ...
-OverflowError: cannot convert float infinity to integer
-
->>> add_integer(0, float('nan'))
-Traceback (most recent call last):
-    ...
-ValueError: cannot convert float NaN to integer!/usr/bin/python3
-""" This module contain a function that adds to integers or float"""
+!/usr/bin/python3
+"""
+This is the "0-add_integer" module.
+The 0-add_integer module supplies one function, add_integer(a, b).
+"""
 
 
-def add_integer(a, b=98):
-    """Returns addition of a and b.
-
-    Raises:
-        TypeError: If either of a or b is not integer  or float.
-    """
-    if ((not isinstance(a, int) and not isinstance(a, float))):
+def add_integer(a, b):
+    """Return the addition of two numbers."""
+    if type(a) is not int and type(a) is not float:
         raise TypeError("a must be an integer")
-    if ((not isinstance(b, int) and not isinstance(b, float))):
+    if type(b) is not int and type(b) is not float:
         raise TypeError("b must be an integer")
-    return (int(a) + int(b))
+    if type(a) is float:
+        a = int(a)
+    if type(b) is float:
+        b = int(b)
+    return a + b
