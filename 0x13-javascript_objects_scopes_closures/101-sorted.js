d4345e4811a757ea1bc52886f0ccfa98a3b0954f
root <rehamhakeem.official@gmail.com>
2024-08-10 08:35:23 -0700

    a small edit

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,13 +1,9 @@
 #!/usr/bin/node
-
 const dict = require('./101-data').dict;
+
 const newDict = {};
 
-Object.keys(dict).map(function (key) {
-  if (!Array.isArray(newDict[dict[key]])) {
-    newDict[dict[key]] = [];
-  }
-  newDict[dict[key]].push(key);
-});
-
+for (const [key, val] of Object.entries(dict)) {
+  newDict[val] ? newDict[val].push(key) : (newDict[val] = [key]);
+}
 console.log(newDict);
